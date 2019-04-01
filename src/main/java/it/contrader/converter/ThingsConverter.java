ccc9231eb867b04af5c5641231b407dc3b3910a7
Debriu <debriu@libero.it>
2019-04-01 15:20:53 +0200

    modifica ThingsConverter

--- src/main/java/it/contrader/converter/ThingsConverter.java
+++ src/main/java/it/contrader/converter/ThingsConverter.java
@@ -1,5 +1,34 @@
 package it.contrader.converter;
 
+import it.contrader.dto.ThingsDTO;
+import it.contrader.model.Things;
+
+
 public class ThingsConverter {
+	/**
+	 * Converte un NodesDTO in Nodes
+	 */
+	public static Things toEntity(ThingsDTO thingDTO) {
+
+		Things thing = null;
+		if (thingDTO != null) {
+			thing = new Things(thingDTO.getId(), thingDTO.getName(), thingDTO.getFktouser(), thingDTO.getFktolabel());
+		}
+
+		return thing;
+	}
+
+	/**
+	 * Converte un Nodes in NodesDTO
+	 */
+	public static ThingsDTO toDTO(Things thing) {
+
+		ThingsDTO thingDTO = null;
+		if (thing != null) {
+			thingDTO = new ThingsDTO(thing.getId(), thing.getName(), thing.getFktouser(), thing.getFktolabel());
+		}
+
+		return thingDTO;
+	}
 
 }
