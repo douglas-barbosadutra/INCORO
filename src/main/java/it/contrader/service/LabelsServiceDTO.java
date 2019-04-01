38cd860b596fbfe6344eb0917317eaddb48c66bf
corinnepepe <corinnepepe01@gmail.com>
2019-04-01 15:03:55 +0200

    modifichee

--- src/main/java/it/contrader/service/LabelsServiceDTO.java
+++ src/main/java/it/contrader/service/LabelsServiceDTO.java
@@ -1,5 +1,57 @@
 package it.contrader.service;
+import java.util.ArrayList;
+import java.util.List;
+
+import it.contrader.converter.LabelsConverter;
+import it.contrader.dao.LabelsDAO;
+import it.contrader.dto.LabelsDTO;
+import it.contrader.model.Labels;
+
+
 
 public class LabelsServiceDTO {
+	private final LabelsDAO LabelsDAO;
 
+	public LabelsServiceDTO() {
+		this.LabelsDAO = new LabelsDAO();
+	}
+
+	/**
+	 * Come vediamo la lista recuperata è di tipo Esempio ma noi la convertiamo in EsempioDTO
+	 * Invito chi fa i converter a fare un metodo per convertire direttamente la lista senza farli uno ad uno perchè è sporco e poco efficiente
+	 */
+	public List<LabelsDTO> getAllLabels() {
+
+		List<Labels> list = LabelsDAO.getAllLabels();
+		List<LabelsDTO> listDTO = new ArrayList<>();
+
+		for (Labels Labels : list) {
+			listDTO.add(LabelsConverter.toDTO(Labels));
+		}
+
+		return listDTO;
+	}
+	
+	
+
+	public boolean updateLabels (LabelsDTO LabelsDTO) {
+		return this.LabelsDAO.updateLabels(LabelsConverter.toEntity(LabelsDTO));
+		
 }
+	
+	public boolean deleteLabels (LabelsDTO LabelsDTO) {
+		return this.LabelsDAO.deleteLabels(LabelsConverter.toEntity(LabelsDTO));
+		
+}
+	
+	public boolean insertLabels (LabelsDTO LabelsDTO) {
+		return this.LabelsDAO.insertLabels(LabelsConverter.toEntity(LabelsDTO));
+	
+}
+		
+	
+	
+}
+
+
+
--- src/main/java/it/contrader/service/ThingsServiceDTO.java
+++ src/main/java/it/contrader/service/ThingsServiceDTO.java
@@ -1,5 +1,56 @@
 package it.contrader.service;
+import java.util.ArrayList;
+import java.util.List;
+
+import it.contrader.converter.ThingsConverter;
+import it.contrader.dao.ThingsDAO;
+import it.contrader.dto.ThingsDTO;
+import it.contrader.model.Things;
+
 
 public class ThingsServiceDTO {
+	private final ThingsDAO ThingsDAO;
+
+	public ThingsServiceDTO() {
+		this.ThingsDAO = new ThingsDAO();
+	}
+
+	/**
+	 * Come vediamo la lista recuperata è di tipo Esempio ma noi la convertiamo in EsempioDTO
+	 * Invito chi fa i converter a fare un metodo per convertire direttamente la lista senza farli uno ad uno perchè è sporco e poco efficiente
+	 */
+	public List<ThingsDTO> getAllThings() {
+
+		List<Things> list = ThingsDAO.getAllThings();
+		List<ThingsDTO> listDTO = new ArrayList<>();
+
+		for (Things Things : list) {
+			listDTO.add(ThingsConverter.toDTO(Things));
+		}
+
+		return listDTO;
+	}
+	
+	public ThingsDTO getThingByname(String name) {
+		return ThingsConverter.toDTO(ThingsDAO.login(name));
+	}
+
+	public boolean updateThings (ThingsDTO ThingsDTO) {
+		return this.ThingsDAO.updateThings(ThingsConverter.toEntity(ThingsDTO));
+		
+}
+	
+	public boolean deleteThings (ThingsDTO ThingsDTO) {
+		return this.ThingsDAO.deleteThings(ThingsConverter.toEntity(ThingsDTO));
+		
+}
+	
+	public boolean insertThings (ThingsDTO ThingsDTO) {
+		return this.ThingsDAO.insertThings(ThingsConverter.toEntity(ThingsDTO));
+	
+}
+		
+	
+	
 
 }
