dcdaa15c609af371433a2ce6c622afcc1b3e579b
Debriu <debriu@libero.it>
2019-05-03 18:17:03 +0200

    User show e delete

--- IncAng/src/app/services/user.service.ts
+++ IncAng/src/app/services/user.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { UserDTO } from 'src/dto/UserDTO';
+import { ConstantPool } from '@angular/compiler';
+//import { UserDTO } from 'dto/UserDTO';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +12,16 @@ export class UserService {
   constructor(private http: HttpClient) { }
   
   insertUser(userDTO: UserDTO){
-    return this.http.post('http.localhost8080/User/insertUser', userDTO);
+    return this.http.post('http://localhost8080/User/insertUser', userDTO);
+  }
+
+  showUser(){
+    return this.http.get('http://localhost:8080/User/showUser');
+  }
+
+  deleteUser(userDTO: UserDTO){
+    console.log(userDTO);
+    return this.http.delete('http://localhost:8080/User/deleteUser?id='+userDTO.idUser);
   }
 
 }
